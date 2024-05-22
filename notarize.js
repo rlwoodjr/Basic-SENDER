eec518c36415ac8a1fc616e2d601ed7f31971092
rlwoodjr <rlwoodjr@gmail.com>
2024-05-22 15:41:37 -0500

    v1.1.38

--- notarize.js
+++ notarize.js
@@ -16,7 +16,7 @@ exports.default = async function notarizing(context) {
 
   try {
     await notarize({
-      appBundleId: "com.bobscnc.LD8F5MWN39",
+      appBundleId: "LD8F5MWN39.basicsender",
       appPath: `${appOutDir}/${appName}.app`,
       appleId: process.env.APPLE_ID,
       appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
