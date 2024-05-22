e3c8d6184ed5be1845ea99c42a4fdd94c7c20a67
rlwoodjr <rlwoodjr@gmail.com>
2024-05-22 07:36:35 -0500

    v1.1.30

--- notarize.js
+++ notarize.js
@@ -1,27 +1,20 @@
 // From https://github.com/simonw/til/blob/main/electron/sign-notarize-electron-macos.md
 // Based on https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
 
-const {
-  notarize
-} = require("@electron/notarize");
+require('dotenv').config();
+const { notarize } = require("@electron/notarize");
 
 exports.default = async function notarizing(context) {
-  const {
-    electronPlatformName,
-    appOutDir
-  } = context;
+  const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== "darwin") return;
-
   const appName = context.packager.appInfo.productFilename;
 
   try {
     await notarize({
-      tool: "notarytool",
-      appBundleId: "LD8F5MWN39.basicsender",
+      appBundleId: 'com.bobscnc.LD8F5MWN39',
       appPath: `${appOutDir}/${appName}.app`,
-      appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
-      teamId: process.env.APPLE_TEAM_ID,
+      appleId: process.env.APPLEID,
+      appleIdPassword: process.env.APPLEIDPASS,
     });
 
     console.log("Notarization using BobsCNC custom notarize.js script: successful");
@@ -29,3 +22,7 @@ exports.default = async function notarizing(context) {
     console.error("Notarization using BobsCNC custom notarize.js script: failed:", error);
   }
 };
+
+
+
+
